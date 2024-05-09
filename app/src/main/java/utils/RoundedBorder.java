4249644cfabd2dce9b262304e747dac237a1bd4a
ddextroo <dexteringuito@gmail.com>
2024-05-09 12:02:08 +0800

    .

---
+++ app/src/main/java/utils/RoundedBorder.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import javax.swing.border.Border;
+
+public class RoundedBorder implements Border {
+
+    private int radius;
+
+    public RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        g2.dispose();
+    }
+
+    // Implement other required methods from Border interface (isEmpty, getBorderInsets)
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(10, 10, 10, 10);
+    }
+}
+
